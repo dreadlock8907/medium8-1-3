b031918cd76de17aa6b75b3c776b0fd7ee421b06
dreadlockie <dreadlock8907@gmail.com>
2020-08-05 14:13:41 +0300

    fixed incapsulation

--- medium8-1-3.cs
+++ medium8-1-3.cs
@@ -1,25 +1,46 @@
-class Bag
-{
-    public List<Item> Items;
-    public int MaxWeidth; 
+  class Bag
+  {
+    public readonly int MaxWeight;
+    List<Item> _items;
+
+    public Bag(int maxWeight)
+    {
+      MaxWeight = maxWeight;
+    }
 
     public void AddItem(string name, int count)
     {
-        int currentWeidth = Items.Sum(item => item.Count);
-        Item targetItem = Items.FirstOrDefault(item => item.Name == name);
+      int currentWeight = _items.Sum(item => item.Count);
+      Item targetItem = _items.FirstOrDefault(item => item.Name == name);
 
-        if (targetItem == null)
-            throw new InvalidOperationException();
+      if (targetItem == null)
+        throw new InvalidOperationException();
 
-        if (currentWeidth + count > MaxWeidth)
-            throw new InvalidOperationException();
+      if (currentWeight + count > MaxWeight)
+        throw new InvalidOperationException();
 
-        targetItem.Count += count;
+      targetItem.IncCount(count);
     }
-}
 
-class Item
-{
-    public int Count;
-    public string Name;
-}
+    public IEnumerable<Item> GetAllItems()
+    {
+      return _items;
+    }
+  }
+
+  class Item
+  {
+    public readonly string Name;
+    public int Count { get; private set; }
+
+    public Item(string name, int count)
+    {
+      Name = name;
+      Count = count;
+    }
+
+    public void IncCount(int count)
+    {
+      Count += count;
+    }
+  }
